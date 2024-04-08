f18babde9ef49337511991d1e0a2d48d92ccf9b3
Savio Junior <72742289+savio85@users.noreply.github.com>
2024-04-08 18:50:21 -0300

    Update atividade-02.c

--- Atividades/atividade-02/atividade-02.c
+++ Atividades/atividade-02/atividade-02.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+  int x;
+  printf("Numeros interiros pares entre 1 e 50:\n");
+  for (x = 2; x <= 50; x++){
+    int rest = x % 2;
+    if ( rest == 0);{
+      printf("%d\n", x);
+    }
+  } 
+  return 0;
+}
