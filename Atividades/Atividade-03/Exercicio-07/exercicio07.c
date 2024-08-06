792e219681491472b9bea4468127d24801ad1739
Savio Junior <72742289+savio85@users.noreply.github.com>
2024-08-06 09:06:16 -0300

    Update exercicio07.c

--- Atividades/Atividade-03/Exercicio-07/exercicio07.c
+++ Atividades/Atividade-03/Exercicio-07/exercicio07.c
@@ -1,1 +1,29 @@
+Algoritmo FritarOvo
 
+Escreva "1. Aqueça a frigideira." 
+Escreva "2. Adicione óleo à frigideira." 
+Escreva "3. Quebre o ovo." 
+Escreva "4. Cozinhe o ovo." 
+Escreva "5. Sirva o ovo." 
+Fim Algoritmo
+
+Algoritmo AtravessarRua
+
+// Instruções para atravessar uma rua com segurança
+
+Escreva "1. Verifique se a rua está segura." 
+Escreva "2. Aguarde o sinal de trânsito." 
+Escreva "3. Atravesse a rua." 
+Escreva "4. Verifique se há carros ou outros obstáculos." 
+Escreva "5. Chegue ao outro lado." 
+Fim Algoritmo
+
+Algoritmo FazerVitaminaDeBanana
+
+Escreva "1. Descasque a banana." 
+Escreva "2. Adicione a banana ao liquidificador."
+Escreva "3. Adicione leite ao liquidificador." 
+Escreva "4. Adicione açúcar ao liquidificador (opcional)." 
+Escreva "5. Bata a mistura até ficar homogênea." 
+Escreva "6. Sirva a vitamina." 
+Fim Algoritmo
